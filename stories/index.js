f3209c4404ebeb8c634d2ae8adefc893a1ac3d28
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-08-04 17:16:12 +0200

    Grids in modals

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,7 @@ storiesOf("Welcome", module).add("to Storybook", () =>
 );
 
 import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
 
 import { Modal, ModalHeader, ModalTitle, ModalBody, ModalFooter } from "../src";
 import { ModalWrapper } from "./modal-wrapper";
@@ -203,6 +204,59 @@ storiesOf("Modal", module)
             cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
             dui. Donec ullamcorper nulla non metus auctor fringilla.
           </p>
+        </ModalBody>
+        <ModalFooter>
+          <button type="button" className="btn btn-secondary">
+            Close
+          </button>
+          <button type="button" className="btn btn-primary">
+            Save changes
+          </button>
+        </ModalFooter>
+      </Modal>
+    </ModalWrapper>
+  )
+  .add("Using the grid", () =>
+    <ModalWrapper>
+      <Modal>
+        <ModalHeader>
+          <ModalTitle>Grids in modals</ModalTitle>
+        </ModalHeader>
+        <ModalBody>
+          <div className="container-fluid bd-example-row">
+            <div className="row">
+              <div className="col-md-4">.col-md-4</div>
+              <div className="col-md-4 col-md-offset-4">
+                .col-md-4 .col-md-offset-4
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-3 col-md-offset-3">
+                .col-md-3 .col-md-offset-3
+              </div>
+              <div className="col-md-2 col-md-offset-4">
+                .col-md-2 .col-md-offset-4
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3">
+                .col-md-6 .col-md-offset-3
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-sm-9">
+                Level 1: .col-sm-9
+                <div className="row">
+                  <div className="col-8 col-sm-6">
+                    Level 2: .col-8 .col-sm-6
+                  </div>
+                  <div className="col-4 col-sm-6">
+                    Level 2: .col-4 .col-sm-6
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </ModalBody>
         <ModalFooter>
           <button type="button" className="btn btn-secondary">
