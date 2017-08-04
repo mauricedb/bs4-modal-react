5ba95940a6d64fc5114f11bc32b47d96d0e78185
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-08-04 16:50:10 +0200

    Usage example

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,53 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-import Example from '../../src'
+import "bootstrap/dist/css/bootstrap.css";
+
+import {
+  Modal,
+  ModalHeader,
+  ModalTitle,
+  ModalBody,
+  ModalFooter
+} from "../../src";
 
 class Demo extends Component {
+  state = {
+    visible: false
+  };
+
+  onShow = () => {
+    this.setState({ visible: true });
+  };
+
+  onHide = () => {
+    this.setState({ visible: false });
+  };
+
   render() {
-    return <div>
-      <h1>bs4-modal-react Demo</h1>
-      <Example/>
-    </div>
+    const { visible } = this.state;
+    return (
+      <div>
+        <button type="button" className="btn btn-primary" onClick={this.onShow}>
+          Launch modal
+        </button>
+        <Modal visible={visible} onHide={this.onHide}>
+          <ModalHeader>
+            <ModalTitle>Modal title</ModalTitle>
+          </ModalHeader>
+          <ModalBody>Woohoo, you're reading this text in a modal!</ModalBody>
+          <ModalFooter>
+            <button type="button" className="btn btn-secondary">
+              Close
+            </button>
+            <button type="button" className="btn btn-primary">
+              Save changes
+            </button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector("#demo"));
