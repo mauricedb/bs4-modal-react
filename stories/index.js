ebef24eef00b7525753bc3b974728424c9e5dffb
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-09-17 21:09:01 +0200

    Removed default example story

--- stories/index.js
+++ stories/index.js
@@ -1,14 +1,5 @@
 import React, { Component } from "react";
-
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
-
-import { Button, Welcome } from "@storybook/react/demo";
-
-storiesOf("Welcome", module).add("to Storybook", () =>
-  <Welcome showApp={linkTo("Button")} />
-);
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
